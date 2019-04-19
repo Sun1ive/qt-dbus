43fe0f88a2f270b85e78c68d2d8af027cda3913d
s.ustinov <s.ustinov@officeukraine.fozzy.lan>
2019-04-19 10:52:42 +0300

    deleted virtual keys

--- main.cpp
+++ main.cpp
@@ -61,8 +61,6 @@ int main(int argc, char *argv[])
 
 
 
-        qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
-
         QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
         QGuiApplication app(argc, argv);
