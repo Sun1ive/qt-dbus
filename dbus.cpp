d250a19a345f0896f1ab3ed0883a4069466f3af1
s.ustinov <s.ustinov@officeukraine.fozzy.lan>
2019-04-24 10:20:16 +0300

    UPD

--- dbus.cpp
+++ dbus.cpp
@@ -56,88 +56,60 @@ QString Dbus::getHostname() const {
     return QHostInfo::localHostName();
 }
 
-QString Dbus::getIp() const {
-    const QHostAddress &localhost = QHostAddress(QHostAddress::LocalHost);
-    QList<QString> list;
-
-    for (const QHostAddress &address: QNetworkInterface::allAddresses()) {
-        if (address.protocol() == QAbstractSocket::IPv4Protocol && address != localhost) {
-            list.append(address.toString());
-        }
-    }
-
-    return list.empty() ? "Нет подключения" : list[0];
-}
-
-
-
 //QString Dbus::getIp() const {
-//    try {
-//    const QDBusConnection systemBus = QDBusConnection::systemBus();
-//    QString service = "org.freedesktop.NetworkManager";
-//    QString servicePath = "/org/freedesktop/NetworkManager";
-//    QString activeConnectionIface = "org.freedesktop.NetworkManager.Connection.Active";
-//    QString ipv4Iface = "org.freedesktop.NetworkManager.IP4Config";
+//    const QHostAddress &localhost = QHostAddress(QHostAddress::LocalHost);
+//    QList<QString> list;
 
-//    //    QDBusInterface *iface = new QDBusInterface("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager",
-//    //                                               "org.freedesktop.NetworkManager", systemBus);
-
-
-
-//    QDBusInterface *iface = new QDBusInterface(service, servicePath, service, systemBus);
-//    QVariant reply = iface->property( "ActiveConnections" );
-//    QDBusObjectPath DBusPath = reply.value<QList<QDBusObjectPath>>()[0];
-//    QString connectionPath = DBusPath.path();
-
-//    qDebug() << connectionPath;
-
-//    QDBusInterface *ifaceActiveConnection = new QDBusInterface(service, connectionPath, activeConnectionIface, systemBus);
-//    QVariant replyActiveConnection = ifaceActiveConnection->property("Ip4Config");
-//    QDBusObjectPath DBusSettingsPath = replyActiveConnection.value<QDBusObjectPath>();
-//    QString ipv4Path = DBusSettingsPath.path();
-
-//    qDebug() << ipv4Path;
-
-//    //    QDBusInterface *ifaceIpv4 = new QDBusInterface(service, ipv4Path, ipv4Iface, systemBus);
-
-////    DBusIP4ConfigInterface *abstractConfig = new DBusIP4ConfigInterface(service, ipv4Path, systemBus);
-
-////    QList<QList<uint>> addresses = abstractConfig->addresses();
-//    /*
-//    qDebug() << addresses[0][0];
-//    qDebug() << addresses[0][1];
-//    qDebug() << addresses[0][2];
-//*/
-//    //    qDebug() << addresses[0];
-
-
-
-
-////    QHostAddress ip = QHostAddress(qFromBigEndian(addresses[0][0]));
-
-////    qDebug() << ip;
-
-//    /*
-//    QVariant replyConnectionUuid = ifaceActiveConnection->property("Uuid");
-//    QVariant replyConnectionType = ifaceActiveConnection->property("Type");
-
-//    QString connectionType = replyConnectionType.toString();
-//    QString connectionUuid = replyConnectionUuid.toString();
-
-//    qDebug () << "TYPE: " << connectionType << " Uuid: " << connectionUuid;
-//*/
-
-////    return ip.toString();
-////    return !addresses.empty() ? QHostAddress(qFromBigEndian(addresses[0][0])).toString() : "No connection";
-
-
-//      } catch (QException error) {
-//        qCritical() << "Error occured: " << error.what();
+//    for (const QHostAddress &address: QNetworkInterface::allAddresses()) {
+//        if (address.protocol() == QAbstractSocket::IPv4Protocol && address != localhost) {
+//            list.append(address.toString());
+//        }
 //    }
 
-//    return "";
-
+//    return list.empty() ? "Нет подключения" : list[0];
 //}
+
+
+
+QString Dbus::getIp() const {
+    const QDBusConnection systemBus = QDBusConnection::systemBus();
+    QString service = "org.freedesktop.NetworkManager";
+    QString servicePath = "/org/freedesktop/NetworkManager";
+    QString activeConnectionIface = "org.freedesktop.NetworkManager.Connection.Active";
+    QString ipv4Iface = "org.freedesktop.NetworkManager.IP4Config";
+
+    QDBusInterface *iface = new QDBusInterface(service, servicePath, service, systemBus);
+    QVariant reply = iface->property( "ActiveConnections" );
+
+    if (reply.value<QList<QDBusObjectPath>>().empty()) {
+        return "Нет подключения";
+    }
+
+    QDBusObjectPath DBusPath = reply.value<QList<QDBusObjectPath>>()[0];
+    QString connectionPath = DBusPath.path();
+
+    qDebug() << connectionPath;
+
+    QDBusInterface *ifaceActiveConnection = new QDBusInterface(service, connectionPath, activeConnectionIface, systemBus);
+    QVariant replyActiveConnection = ifaceActiveConnection->property("Ip4Config");
+    QDBusObjectPath DBusSettingsPath = replyActiveConnection.value<QDBusObjectPath>();
+    QString ipv4Path = DBusSettingsPath.path();
+
+    qDebug() << ipv4Path;
+
+    DBusIP4ConfigInterface *abstractConfig = new DBusIP4ConfigInterface(service, ipv4Path, systemBus);
+    QList<QList<uint>> addresses = abstractConfig->addresses();
+
+    if (addresses.empty()) {
+        return "Нет подключения";
+    }
+
+    QHostAddress ip = QHostAddress(qFromBigEndian(addresses[0][0]));
+
+    qDebug() << ip;
+
+    return ip.toString();
+}
 
 
 uint Dbus::getState() const {
@@ -160,7 +132,7 @@ void Dbus::setState(const uint state) {
     qDebug() << "state changed:\t" << state;
 
     this->m_state = state;
-//    this->m_ip = this->getIp();
+    this->m_ip = this->getIp();
     this->m_hostname = this->getHostname();
 
     emit stateChanged();
